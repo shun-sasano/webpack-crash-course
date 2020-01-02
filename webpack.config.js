a96da52085b805dd621fdb707802948396f98c79
shun-sasano <jumpjudo3303@gmail.com>
2020-01-03 00:08:06 +0900

    install webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,11 @@ const outputPath = path.resolve(__dirname, 'dist')
 console.log({outputPath})
 module.exports = {
   entry: './src/index.js', // バンドル対象の設定
-  output: {
+  output: { // バンドル結果のjsファイルの名前とファイルの設置場所の設定
     filename: 'main.js',
     path: outputPath
+  },
+  devServer: { // ドキュメントルートの設定。
+    contentBase: outputPath
   }
 }
