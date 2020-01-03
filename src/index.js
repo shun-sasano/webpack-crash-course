c6bf74b59b62881665b517cc485e9f0be174c231
shun-sasano <jumpjudo3303@gmail.com>
2020-01-03 10:45:22 +0900

    implement utilities

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import _ from 'lodash'; 
+
+// 関数のimportは{}でかこう。
+// import { NAME, NiJou } from './utilities' // .jsは省略可能
+// console.log(NiJou(22));
+// console.log(NAME);
+// import * as utilities from './utilities'
+// console.log(utilities.NiJou(222));
+// console.log(utilities.NAME);
+// import {NAME as NAME_OF_HAM} from './utilities'
+// console.log(NAME_OF_HAM);
+
+import Tiger from './utilities'
+console.log(Tiger.say());
+
 function component() {
   const element = document.createElement('div');
   const array = ["Hello", 'webpack'];
